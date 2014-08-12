14f3590ed8921167bf5bde78e8fc6c53e41b3891
epyatopal <pyatopal@yahoo.com>
2014-08-12 18:42:14 +0400

    add send email

--- routes/auth.js
+++ routes/auth.js
@@ -5,7 +5,15 @@ var express = require('express')
     , TwitterStrategy = require('passport-twitter').Strategy
     , passport = require('passport')
     , config = require('../config/config')
+    , nodemailer = require("nodemailer")
 
+    , smtpTransport = nodemailer.createTransport("SMTP", {
+        service: 'Gmail',
+        auth: {
+            user: 'translate2222@gmail.com',
+            pass: 'web2222inter'
+        }
+    })
 //router.post('/session-save', function(req, res) {
 //    console.log('>>>>>>>>>', req.body)
 //    res.send(200)
@@ -19,11 +27,26 @@ router.get('/facebook', function (req, res, next) {
     passport.authenticate('facebook')(req, res, next)
     });
 
-router.get('/facebook/callback',function (req, res, next) {
-    passport.authenticate('facebook', { successRedirect: '/allOK',
-            failureRedirect: '/auth'}
-    )(req, res, next)
-});
+router.get('/facebook/callback',
+    passport.authenticate('facebook', { failureRedirect: '/auth' }),
+    function(req, res) {
+        var mailOptions = {
+            from: 'translate2222@gmail.com', // sender address
+            to: 'translate2222@gmail.com', // list of receivers
+            subject: '/facebook/callback success', // Subject line
+            text: 'FacebookStrategy success' // plaintext body
+        };
+
+        smtpTransport.sendMail(mailOptions, function(error, info){
+            if(error){
+                console.log('mail FacebookStrategy success error: ', error);
+            }else{
+                console.log('Message sent: ' + info.response);
+            }
+        });
+        // Successful authentication, redirect home.
+        res.redirect('/allOK');
+    });
 
 //router.get('/google', function (req, res, next) {
 //    passport.authenticate('google')(req, res, next)
@@ -42,11 +65,26 @@ router.get('/facebook/callback',function (req, res, next) {
 router.get('/google',
     passport.authenticate('google', { scope: 'https://www.google.com/m8/feeds' }));
 
-router.get('/google/callback',function (req, res, next) {
-        passport.authenticate('google', { successRedirect: '/allOK',
-                failureRedirect: '/auth'}
-    )(req, res, next)
-});
+router.get('/google/callback',
+    passport.authenticate('google', { failureRedirect: '/auth' }),
+    function(req, res) {
+        var mailOptions = {
+            from: 'translate2222@gmail.com', // sender address
+            to: 'translate2222@gmail.com', // list of receivers
+            subject: '/google/callback success', // Subject line
+            text: 'GoogleStrategy success' // plaintext body
+        };
+
+        smtpTransport.sendMail(mailOptions, function(error, info){
+            if(error){
+                console.log('mail GoogleStrategy success error: ', error);
+            }else{
+                console.log('Message sent: ' + info.response);
+            }
+        });
+        // Successful authentication, redirect home.
+        res.redirect('/allOK');
+    });
 
 
 router.get('/twitter',
@@ -55,6 +93,20 @@ router.get('/twitter',
 router.get('/twitter/callback',
     passport.authenticate('twitter', { failureRedirect: '/auth' }),
     function(req, res) {
+        var mailOptions = {
+            from: 'translate2222@gmail.com', // sender address
+            to: 'translate2222@gmail.com', // list of receivers
+            subject: '/twitter/callback success', // Subject line
+            text: 'TwitterStrategy success' // plaintext body
+        };
+
+        smtpTransport.sendMail(mailOptions, function(error, info){
+            if(error){
+                console.log('mail TwitterStrategy success error: ', error);
+            }else{
+                console.log('Message sent: ' + info.response);
+            }
+        });
         // Successful authentication, redirect home.
         res.redirect('/allOK');
     });
@@ -64,6 +116,22 @@ router.get('/twitter/callback',
 
 
 router.get('/logout', function(req, res){
+    // setup e-mail data with unicode symbols
+    var mailOptions = {
+        from: 'translate2222@gmail.com', // sender address
+        to: 'translate2222@gmail.com', // list of receivers
+        subject: 'logout', // Subject line
+        text: 'user ' + req.user + ' logout' // plaintext body
+    };
+
+// send mail with defined transport object
+    smtpTransport.sendMail(mailOptions, function(error, info){
+        if(error){
+            console.log('mail logout error: ', error);
+        }else{
+            console.log('Message sent: ' + info.response);
+        }
+    });
         req.session.passport = {}
         res.redirect('/');
 
@@ -78,6 +146,21 @@ router.get('/logout', function(req, res){
 //            passReqToCallback: true
         },
         function (accessToken, refreshToken, profile, done) {
+            var mailOptions = {
+                from: 'translate2222@gmail.com', // sender address
+                to: 'translate2222@gmail.com', // list of receivers
+                subject: 'FacebookStrategy', // Subject line
+                text: 'FacebookStrategy done: ' + profile // plaintext body
+            };
+
+// send mail with defined transport object
+            smtpTransport.sendMail(mailOptions, function(error, info){
+                if(error){
+                    console.log('mail FacebookStrategy done: ', error);
+                }else{
+                    console.log('Message sent: ' + info.response);
+                }
+            });
             done(null, profile)
         }
     ));
@@ -88,6 +171,21 @@ passport.use(new GoogleStrategy({
         callbackURL: "http://127.0.0.1:3000/auth/google/callback"
     },
     function(accessToken, refreshToken, profile, done) {
+        var mailOptions = {
+            from: 'translate2222@gmail.com', // sender address
+            to: 'translate2222@gmail.com', // list of receivers
+            subject: 'GoogleStrategy', // Subject line
+            text: 'GoogleStrategy done: ' + profile // plaintext body
+        };
+
+// send mail with defined transport object
+        smtpTransport.sendMail(mailOptions, function(error, info){
+            if(error){
+                console.log('mail GoogleStrategy done: ', error);
+            }else{
+                console.log('Message sent: ' + info.response);
+            }
+        });
         done(null, profile)
     }
 ));
@@ -98,6 +196,21 @@ passport.use(new TwitterStrategy({
         callbackURL: "http://127.0.0.1:3000/auth/twitter/callback"
     },
     function(token, tokenSecret, profile, done) {
+        var mailOptions = {
+            from: 'translate2222@gmail.com', // sender address
+            to: 'translate2222@gmail.com', // list of receivers
+            subject: 'TwitterStrategy', // Subject line
+            text: 'TwitterStrategy done: ' + profile // plaintext body
+        };
+
+// send mail with defined transport object
+        smtpTransport.sendMail(mailOptions, function(error, info){
+            if(error){
+                console.log('mail TwitterStrategy done: ', error);
+            }else{
+                console.log('Message sent: ' + info.response);
+            }
+        });
         done(null, profile)
     }
 ));
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,47 @@
 var express = require('express');
-var router = express.Router();
-
+var router = express.Router()
+, nodemailer = require("nodemailer")
+, smtpTransport = nodemailer.createTransport("SMTP", {
+    service: 'Gmail',
+    auth: {
+        user: 'translate2222@gmail.com',
+        pass: 'web2222inter'
+    }
+})
 /* GET home page. */
 router.get('/', function(req, res) {
+    var mailOptions = {
+        from: 'translate2222@gmail.com', // sender address
+        to: 'translate2222@gmail.com', // list of receivers
+        subject: 'show index', // Subject line
+        text: 'show index ' + req.user // plaintext body
+    };
+
+    smtpTransport.sendMail(mailOptions, function(error, info){
+        if(error){
+            console.log('mail show index error: ', error);
+        }else{
+            console.log('Message sent: ' + info.response);
+        }
+    });
   res.render('index', { title: 'Express' });
 });
 
 router.get('/allOK', function(req, res) {
+    var mailOptions = {
+        from: 'translate2222@gmail.com', // sender address
+        to: 'translate2222@gmail.com', // list of receivers
+        subject: 'show allOK', // Subject line
+        text: 'show allOK ' + req.user // plaintext body
+    };
+
+    smtpTransport.sendMail(mailOptions, function(error, info){
+        if(error){
+            console.log('mail show allOK error: ', error);
+        }else{
+            console.log('Message sent: ' + info.response);
+        }
+    });
     res.render('allOK', { title: 'Express', user: req.user });
 });
 
