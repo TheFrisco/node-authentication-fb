4baee68867fd0f651532d32db9bc9a3236a056f2
Robert Schmid <robert@theschmids.org>
2014-08-12 23:27:28 -0700

    Updated FB App Tokens

--- config/auth.js
+++ config/auth.js
@@ -4,23 +4,23 @@
 module.exports = {
     development: {
         'facebookAuth' : {
-            'clientID' 		: '807455782623130', // your App ID
-            'clientSecret' 	: '07b623a8ca695cfe590f5bbc9234a3bc', // your App Secret
+            'clientID' 		: '755839674472722', // your App ID
+            'clientSecret' 	: '7f468a1b168dae9ee71ff1553ffca647', // your App Secret
             'callbackURL' 	: 'http://localhost:8080/auth/facebook/callback'
         }
     },
     staging: {
         'facebookAuth' : {
-            'clientID': '807455782623130', // your App ID
-            'clientSecret': '07b623a8ca695cfe590f5bbc9234a3bc', // your App Secret
-            'callbackURL': 'http://localhost:8080/auth/facebook/callback'
+            'clientID': '348246538658784', // your App ID
+            'clientSecret': '082e7b116f91a3766b4fa78903329595', // your App Secret
+            'callbackURL': 'http://passport-test-rsc.herokuapp.com/auth/facebook/callback'
         }
     },
     qa: {
         'facebookAuth' : {
-            'clientID': '807455782623130', // your App ID
-            'clientSecret': '07b623a8ca695cfe590f5bbc9234a3bc', // your App Secret
-            'callbackURL': 'http://localhost:8080/auth/facebook/callback'
+            'clientID': '931104163573504', // your App ID
+            'clientSecret': 'edd4ea595099a691670bfe73c610ec15', // your App Secret
+            'callbackURL': 'http://oakatpqa.bamnetworks.com/auth/facebook/callback'
         }
     }
 };
